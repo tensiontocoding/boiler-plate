4393bec876bf4d96c8ac3ab1afc04138929399eb
tensiontocoding <91064624+tensiontocoding@users.noreply.github.com>
2023-03-07 18:47:31 +0900

    .

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5001
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require("./models/Users");
 
 //application/x-www-form-urlencoded
@@ -10,13 +13,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dobbyk52:dudxo031@boilerplate.jfizbti.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! hi hello ')
 })
 
 app.post('/register', (req, res) => {
@@ -26,7 +29,7 @@ app.post('/register', (req, res) => {
     // user.save((err, doc) => {
     //     if (err) return res.json({ success: false, err })
     //     return res.status(200).json({
-    //         success: truex
+    //         success: true
     //     })
     // })
     user.save().then(() => {
