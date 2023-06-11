c589c04878d5089ec1a018b18cfaac039a4fe357
tensiontocoding <91064624+tensiontocoding@users.noreply.github.com>
2023-06-11 23:45:58 +0900

    readme 수정

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # boiler-plate
+
+## 목적
+
+기초 개발 방법 완전 숙지
+
+## 개념
+
+- 기본 node 서버 구성 & react 클라이언트 구성
+- mongoDB 연결 & Auth 개념 숙지
+- 기본 라이브러리 사용버ㅂ
+    experess, mongoose, bcrypt, concurrently, cookie-parser, jsonwebtoken, nodemon
+- git & github 사용
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,7 +5,7 @@ let auth = (req, res, next) => {
 
     //클라이언트 쿠키에서 토큰을 가져온다.
     let token = req.cookies.x_auth;
-    //토큰 복호화 후 유적 찾기
+    //토큰 복호화 후 유저 찾기
     User.findByToken(token, (err, user) => {
         if (err) throw err;
         if (!user) return res.json({ isAuth: false, error: true })
